f829eeb69dfce963eff983a4a53e3b2c86a802b8
Sumeet Sharma <sksksharma0@gmail.com>
2019-12-04 15:17:50 +0530

    Adding includes files

---
+++ includes/left.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>Sumeet Sharma</title>
+  <!-- MDB icon -->
+  <link rel="icon" href="img/mdb-favicon.ico" type="image/x-icon">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
+  <!-- Bootstrap core CSS -->
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <!-- Material Design Bootstrap -->
+  <link rel="stylesheet" href="css/mdb.min.css">
+  <!-- Your custom styles (optional) -->
+  <link rel="stylesheet" href="css/style.css">
+
+  <link href="https://fonts.googleapis.com/css?family=Nunito|Poppins|Righteous|Varela+Round&display=swap"
+    rel="stylesheet">
+
+
+  <style type="text/css">
+    * {
+      border: 0;
+      padding: 0;
+      margin: 0;
+    }
+
+    html,
+    body {
+      overflow-x: hidden
+    }
+  </style>
+</head>
+
+<body>
+
+  <div class="row">
+    <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 pr-0 fixed" style="background-color:#fff;">
+      <div class="profile">
+        <h6 class="pt-3 pb0 pl-3 pr-3 h6-responsive text-justify" style="font-family: Poppins;">Hello everyone! I am
+        </h6>
+        <h2 class="pl-3 pr-3 pt-0 h2-responsive text-justify" style="font-family: Righteous;text-transform: uppercase">
+          Sumeet Sharma</h2>
+      </div>
+      <div class="profile-image mx-3">
+        <img src="http://netmatesolutions.in/portfolio/img/sumeet.jpg"
+          style="width: 50%;box-shadow: 2px 2px 10px 0px rgba(0,0,0,0.5);">
+      </div>
+      <div class="profile-statement text-justify">
+        <p class="pt-3 px-3 pb-1" style="line-height: 1.6em;font-weight:500">
+          Graduate Engineering Trainee | Web Developer | WordPress Developer
+        </p>
+      </div>
+      <div class="social-links pl-3 pr-3">
+        <a href="#"><i class="fab fa-facebook-f pr-2 pt-2 pb-2" style="font-size: 26px;color:#222"></i></a>
+        <a href="#"><i class="fab fa-twitter p-2" style="font-size: 24px;color:#222"></i></a>
+        <a href="#"><i class="fab fa-linkedin p-2" style="font-size: 24px;color:#222"></i></a>
+        <a href="#"><i class="fab fa-instagram p-2" style="font-size: 24px;color:#222"></i></a>
+        <a href="#"><i class="fab fa-github p-2" style="font-size: 24px;color:#222"></i></a>
+      </div>
+      <!-- Grid row -->
+<div class="">
+
+  <nav class="nav flex-column dark lighten-3 py-4 mb-r font-weight-bold z-depth-0">
+    <a class="nav-link active dark-grey-text" href="technologies.php">Technologies</a>
+    <a class="nav-link dark-grey-text" href="education.php">Education</a>
+    <a class="nav-link dark-grey-text" href="portfolio.php">Portfolio</a>
+    <a class="nav-link dark-grey-text" href="experience.php">Experience</a>
+  </nav>
+
+</div>
+<!-- Grid row -->
+      <div class="technologies-progress-bar">
+        <h5 class="h5-responsive pl-3 mt-4"><b>HTML5</b><i class="fab fa-html5 ml-2"
+            style="font-size: 20px;color:#222"></i></h5>
+        <div class="progress md-progress mx-3" style="height: 20px">
+          <div class="progress-bar" role="progressbar" style="width: 90%; background: #F16529; height: 20px"
+            aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">90%</div>
+        </div>
+        <h5 class="h5-responsive pl-3 mt-3"><b>CSS3</b><i class="fab fa-css3 ml-2"
+            style="font-size: 20px;color:#222"></i></h5>
+        <div class="progress md-progress mx-3" style="height: 20px">
+          <div class="progress-bar" role="progressbar" style="width: 85%; background: #264de4; height: 20px"
+            aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">85%</div>
+        </div>
+        <h5 class="h5-responsive pl-3 mt-3"><b>JavaScript</b><i class="fab fa-js ml-2"
+            style="font-size: 20px;color:#222"></i></h5>
+        <div class="progress md-progress mx-3" style="height: 20px">
+          <div class="progress-bar" role="progressbar" style="width: 70%; background: #323330; height: 20px"
+            aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">70%</div>
+        </div>
+        <h5 class="h5-responsive pl-3 mt-3"><b>Bootstrap</b><i class="fab fa-bootstrap ml-2"
+            style="font-size: 20px;color:#222"></i></h5>
+        <div class="progress md-progress mx-3" style="height: 20px">
+          <div class="progress-bar" role="progressbar" style="width: 85%; background: #080135; height: 20px"
+            aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">85%</div>
+        </div>
+        <h5 class="h5-responsive pl-3 mt-3"><b>PHP</b><i class="fab fa-php ml-2" style="font-size: 20px;color:#222"></i>
+        </h5>
+        <div class="progress md-progress mx-3" style="height: 20px">
+          <div class="progress-bar" role="progressbar" style="width: 70%; background: #787CB5; height: 20px"
+            aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">70%</div>
+        </div>
+        <h5 class="h5-responsive pl-3 mt-3"><b>WordPress</b><i class="fab fa-wordpress ml-2"
+            style="font-size: 20px;color:#222"></i></h5>
+        <div class="progress md-progress mx-3" style="height: 20px">
+          <div class="progress-bar" role="progressbar" style="width: 85%; background: #21759b; height: 20px"
+            aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">85%</div>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-9 col-md-9 col-sm-8 col-xs-12 pr-0 pl-0">
